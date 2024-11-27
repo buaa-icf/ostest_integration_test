cfa2b7586c8dd4e7984b0810082b5c7244428800
sunlian <sunlian@kaihong.com>
2024-11-27 14:49:59 +0800

    增加cpu性能测试case
    Signed-off-by: sunlian <sunlian@kaihong.com>

---
+++ OHBM/entry/src/main/ets/cases/BenchmarkMeasure.ts
@@ -0,0 +1,190 @@
+/*
+ * Copyright (c) 2023 Shenzhen Kaihong Digital Industry Development Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { ThreadWorkerGlobalScope } from '@kit.ArkTS';
+
+const TAG = 'Benchmark'
+
+class BenchmarkRNG {
+  static resetRNG() {
+    Math.random = (function () {
+      var seed = 49734321;
+      return function () {
+        seed = ((seed + 0x7ed55d16) + (seed << 12)) & 0xffffffff;
+        seed = ((seed ^ 0xc761c23c) ^ (seed >>> 19)) & 0xffffffff;
+        seed = ((seed + 0x165667b1) + (seed << 5)) & 0xffffffff;
+        seed = ((seed + 0xd3a2646c) ^ (seed << 9)) & 0xffffffff;
+        seed = ((seed + 0xfd7046c5) + (seed << 3)) & 0xffffffff;
+        seed = ((seed ^ 0xb55a4f09) ^ (seed >>> 16)) & 0xffffffff;
+        return (seed & 0xfffffff) / 0x10000000;
+      };
+    })();
+  }
+
+  static nextDouble() {
+    return Math.random()
+  }
+}
+
+class WorkerMessage {
+  name: string;
+  type: number;
+  time: number;
+  latency: number;
+  x: number;
+  y: number;
+  r: number;
+  g: number;
+  b: number;
+
+  constructor(name: string, type: number, time: number, latency: number, x: number, y: number, r: number, g: number,
+    b: number) {
+    this.name = name;
+    this.type = type;
+    this.time = time;
+    this.latency = latency;
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.g = g;
+    this.b = b;
+  }
+}
+
+class BenchmarkResult {
+  time: number;
+  latency: number;
+
+  constructor(time: number, latency: number) {
+    this.time = time;
+    this.latency = latency;
+  }
+}
+
+class Benchmark {
+  name: string;
+  doWarmup: boolean;
+  doDeterministic: boolean;
+  run: Function;
+  setup: Function | undefined;
+  tearDown: Function | undefined;
+  rmsResult: Function | undefined;
+  minIterations: number;
+  data: {
+    runs: number,
+    elapsed: number
+  } | undefined;
+
+  constructor(name: string, doWarmup: boolean, doDeterministic: boolean, run: Function,
+    setup?: Function, tearDown?: Function, rmsResult?: Function, minIterations?: number) {
+    this.name = name;
+    this.doWarmup = doWarmup;
+    this.doDeterministic = doDeterministic;
+    this.run = run;
+    this.setup = setup;
+    this.tearDown = tearDown
+    this.rmsResult = rmsResult
+    this.minIterations = minIterations ? minIterations : 32;
+    this.data = undefined
+  }
+
+  runSetup(benchmarkResult: BenchmarkResult, workerPort: ThreadWorkerGlobalScope): void {
+    this.setup?.();
+    this.runBenchmark(benchmarkResult, workerPort);
+  }
+
+  runBenchmark(benchmarkResult: BenchmarkResult, workerPort: ThreadWorkerGlobalScope): void {
+    this.runSingle(benchmarkResult, workerPort);
+    !this.data ? this.runTearDown() : this.runBenchmark(benchmarkResult, workerPort);
+  }
+
+  runTearDown(): void {
+    this.tearDown?.();
+  }
+
+  runSingle(benchmarkResult: BenchmarkResult, workerPort: ThreadWorkerGlobalScope): void {
+    if (!this.doWarmup && !this.data) {
+      this.data = { runs: 0, elapsed: 0 };
+    }
+
+    if (!this.data) {
+      this.measure(workerPort);
+      this.data = { runs: 0, elapsed: 0 };
+    } else {
+      this.measure(workerPort);
+      if (this.data.runs < this.minIterations) {
+        return;
+      }
+      const usec: number = (this.data.elapsed * 1000) / this.data.runs;
+      const rms: number = (this.rmsResult != null) ? this.rmsResult() : 0;
+      benchmarkResult.time = usec;
+      benchmarkResult.latency = rms;
+      this.data = undefined;
+    }
+  }
+
+  measure(workerPort: ThreadWorkerGlobalScope): void {
+    let elapsed: number = 0;
+    let start: number = new Date().getTime();
+
+    let i = 0
+    for (; (this.doDeterministic ? i < this.minIterations : elapsed < 1000); i++) {
+      this.run(workerPort);
+      elapsed = new Date().getTime() - start;
+      workerPort.postMessage(new WorkerMessage(this.name+`(第${i + 1}次)`, 0, elapsed, 0, 0, 0, 0, 0, 0))
+    }
+    if (this.data != null) {
+      this.data.runs += i;
+      this.data.elapsed += elapsed;
+    }
+  }
+}
+
+
+class BenchmarkRun {
+  benchmark: Benchmark
+  name:string
+
+  constructor(name: string, doWarmup: boolean, doDeterministic: boolean, run: Function,
+    setup?: Function, tearDown?: Function, rmsResult?: Function, minIterations?: number) {
+    this.benchmark = new Benchmark(name, doWarmup, doDeterministic, run, setup, tearDown, rmsResult, minIterations)
+    this.name = name
+  }
+
+  run(workerPort: ThreadWorkerGlobalScope) {
+    let result = new BenchmarkResult(0, 0)
+    try {
+      BenchmarkRNG.resetRNG()
+      this.benchmark.runSetup(result, workerPort)
+      this.printResult(result)
+      workerPort.postMessage(new WorkerMessage(this.name+'(完成)', 0, result.time, result.latency, 0, 0, 0, 0, 0))
+    } catch (error) {
+      this.printErrorMessage(JSON.stringify(error))
+    }
+
+  }
+
+  printErrorMessage(errorMessage: String) {
+    console.info(TAG, `${this.benchmark.name} Error: ${errorMessage}`)
+  }
+
+  printResult(result: BenchmarkResult) {
+    console.info(TAG,
+      `${this.benchmark.name}: usec = ${result.time}\n${this.benchmark.name}: latency = ${result.latency}`)
+  }
+}
+
+BenchmarkRNG.resetRNG()
+
+export { BenchmarkRun, Benchmark, BenchmarkResult, BenchmarkRNG, WorkerMessage }
---
+++ OHBM/entry/src/main/ets/cases/performance/base64.ts
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2023 Shenzhen Kaihong Digital Industry Development Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { BenchmarkRun } from "../BenchmarkMeasure";
+
+
+/**************************source code********************************/
+
+const toBase64Table: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+const base64Pad: string = '=';
+
+const toBinaryTable: number[] = [
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63,
+  52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, 0, -1, -1,
+  -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+  15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1,
+  -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+  41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1
+];
+
+function base64ToString(data: string): string {
+  let result: string = '';
+  let leftbits: number = 0;
+  let leftdata: number = 0;
+
+  for (let i = 0; i < data.length; i++) {
+    let c: number = toBinaryTable[data.charCodeAt(i) & 0x7f];
+    let padding: boolean = (data.charCodeAt(i) == base64Pad.charCodeAt(0));
+    if (c == -1) {
+      continue;
+    }
+    leftdata = (leftdata << 6) | c;
+    leftbits += 6;
+    if (leftbits >= 8) {
+      leftbits -= 8;
+      if (!padding) {
+        result += String.fromCharCode((leftdata >> leftbits) & 0xff);
+      }
+      leftdata &= (1 << leftbits) - 1;
+    }
+  }
+  if (leftbits) {
+    throw new Error('Corrupted base64 string');
+  }
+  return result;
+}
+
+
+function toBase64(data: string): string {
+  let result: string = '';
+  const length: number = data.length;
+  let i: number;
+
+  for (i = 0; i < (length - 2); i += 3) {
+    result += toBase64Table[data.charCodeAt(i) >> 2];
+    result += toBase64Table[((data.charCodeAt(i) & 0x03) << 4) + (data.charCodeAt(i + 1) >> 4)];
+    result += toBase64Table[((data.charCodeAt(i + 1) & 0x0f) << 2) + (data.charCodeAt(i + 2) >> 6)];
+    result += toBase64Table[data.charCodeAt(i + 2) & 0x3f];
+  }
+  if (length % 3) {
+    i = length - (length % 3);
+    result += toBase64Table[data.charCodeAt(i) >> 2];
+    if ((length % 3) == 2) {
+      result += toBase64Table[((data.charCodeAt(i) & 0x03) << 4) + (data.charCodeAt(i + 1) >> 4)];
+      result += toBase64Table[(data.charCodeAt(i + 1) & 0x0f) << 2];
+      result += base64Pad;
+    } else {
+      result += toBase64Table[(data.charCodeAt(i) & 0x03) << 4];
+      result += base64Pad + base64Pad;
+    }
+  }
+  return result;
+}
+
+
+function Base64Run(): void {
+  let str: string = "";
+  for (let i = 0; i < 8192; i++) {
+    str += String.fromCharCode((25 * Math.random()) + 97);
+  }
+  for (let i: number = 8192; i <= 16384; i *= 2) {
+    let base64: string;
+    base64 = toBase64(str);
+    let encoded: string = base64ToString(base64);
+    if (encoded !== str) {
+      throw new Error("ERROR: bad result: expected " + str + " but got " + encoded);
+    }
+    // Double the string
+    str += str;
+  }
+}
+
+
+/**************************configure and run benchmark********************************/
+export { Base64Run }
